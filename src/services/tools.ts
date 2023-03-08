424b493b304f96f5380449888d354ebbec6c967c
Victor MOIN <Victor.MOIN1@ibm.com>
2023-03-08 15:20:48 +0100

    Delete tools.ts

--- src/services/tools.ts
+++
@@ -1,9 +0,0 @@
-function addition(x: number, y: number): number {
-    return x + y;
-}
-
-function subtraction(x: number, y: number): number {
-    return x - y;
-}
-
-export { addition, subtraction };
